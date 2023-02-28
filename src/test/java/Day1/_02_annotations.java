2da98c869f6ef33210253579ffaecaebc71648db
asma <asma@portakal.net>
2023-02-28 12:59:33 -0800

    day1 expl2

--- src/test/java/Day1/_02_annotations.java
+++ src/test/java/Day1/_02_annotations.java
@@ -1,14 +1,18 @@
 package Day1;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class _02_annotations {
     //beforemethod:this code will run before all test annotations
     //we use annotations to write our tests
     //aftermethod:this code will run after annotations
     //afterclass:code will be run after the class
+    @BeforeClass
+    void beforeClass() {
+        System.out.println("before class is working");
+    }
+
+
     @BeforeMethod
     void beforeMethod(){
         System.out.println("before method is working");
@@ -16,6 +20,10 @@ public class _02_annotations {
     @AfterMethod
     void afterMethod(){
         System.out.println("after method is working");
+    }
+    @AfterClass
+    void afterclass(){
+        System.out.println("after class is working");
     }
     @Test
      void test1 (){
